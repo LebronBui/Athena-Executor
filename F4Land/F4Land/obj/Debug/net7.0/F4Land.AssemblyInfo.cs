32460f0ece490c548048f45878b851a53f2b8813
toannnse171130 <155402953+toannnse171130@users.noreply.github.com>
2024-02-24 01:08:04 +0700

    update paymentDao

--- F4Land/F4Land/obj/Debug/net7.0/F4Land.AssemblyInfo.cs
+++ F4Land/F4Land/obj/Debug/net7.0/F4Land.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("F4Land")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+178de1bc4d8c556647f04aa3dba43f219542bdae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3ad3500a80575a44597eb19ee37078523007c3b")]
 [assembly: System.Reflection.AssemblyProductAttribute("F4Land")]
 [assembly: System.Reflection.AssemblyTitleAttribute("F4Land")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
