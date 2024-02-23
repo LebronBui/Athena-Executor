579bbc2aadd3af728a2d7df08c1b046c8a694ffa
toannnse171130 <155402775+kiettase171131@users.noreply.github.com>
2024-02-23 22:56:44 +0700

    update home controller

--- F4Land/F4Land/Controllers/HomeController.cs
+++ F4Land/F4Land/Controllers/HomeController.cs
@@ -1,6 +1,6 @@
-﻿using Humanizer;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using RealEstateAuction.DAL;
+using RealEstateAuction.DataModel;
 using RealEstateAuction.Models;
 using System.Diagnostics;
 
@@ -9,22 +9,68 @@ namespace RealEstateAuction.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AuctionDAO auctionDAO;
+        private readonly Pagination pagination;
 
         public HomeController(ILogger<HomeController> logger)
         {
-            _logger = logger;
+            pagination = new Pagination();
+           _logger = logger;
         }
 
         [Route("")]
         [Route("home")]
         public IActionResult Index()
         {
-            return View();
-            / get 5 auction recently to display on hompage
+            //get 5 auction recently to display on hompage
             List<Auction> auctionRecent = auctionDAO.GetAuctionRecently(5);
 
+            return View(auctionRecent);
         }
 
+        [Route("list-auction")]
+        public IActionResult ListAuction(int? pageNumber)
+        {
+            if (pageNumber.HasValue)
+            {
+                pagination.PageNumber = pageNumber.Value;
+            }
+
+            //get all auction approved to display on list auction page
+            List<Auction> auctions = auctionDAO.GetAllAuctionApproved(pagination);
+
+            int auctionCount = auctionDAO.CountAuctionApproved();
+            int pageSize = (int)Math.Ceiling((double)auctionCount / pagination.RecordPerPage);
+
+            ViewBag.currentPage = pagination.PageNumber;
+            ViewBag.pageSize = pageSize;
+
+            return View(auctions);
+        }
+
+        [Route("auction-details")]
+        public IActionResult AuctionDetails(int? auctionId)
+        {
+            //check auctionId is null or not
+            if (auctionId.HasValue)
+            {
+                Auction auction = auctionDAO.GetAuctionById(auctionId.Value);
+                //check auction is found or not
+                if (auction != null)
+                {
+                    return View(auction);
+                }
+                else
+                {
+                    TempData["Message"] = "Không tìm thấy phiên đấu giá này";
+                    return RedirectToAction("ListAuction");
+                }
+            }
+            else
+            {
+                return RedirectToAction("ListAuction");
+            }
+        }
 
         public IActionResult Privacy()
         {
