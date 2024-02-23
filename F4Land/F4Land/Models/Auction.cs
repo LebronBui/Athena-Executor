8a91483fcca0ddac5962ed758a07052f536096bd
toannnse171130 <155402953+toannnse171130@users.noreply.github.com>
2024-02-24 00:41:13 +0700

    update models

--- F4Land/F4Land/Models/Auction.cs
+++ F4Land/F4Land/Models/Auction.cs
@@ -3,49 +3,17 @@ using System.Collections.Generic;
 
 namespace RealEstateAuction.Models;
 
-public partial class Auction
+public partial class Banking
 {
     public int Id { get; set; }
 
-    public string Title { get; set; } = null!;
+    public string? BankAccount { get; set; }
 
-    public decimal StartPrice { get; set; }
+    public string? BankName { get; set; }
 
-    public decimal EndPrice { get; set; }
+    public bool? Status { get; set; }
 
-    public double Area { get; set; }
+    public bool? DeleteFlag { get; set; }
 
-    public string Address { get; set; } = null!;
-
-    public double Facade { get; set; }
-
-    public string Direction { get; set; } = null!;
-
-    public string Description { get; set; } = null!;
-
-    public int UserId { get; set; }
-
-    public int ApproverId { get; set; }
-
-    public byte Status { get; set; }
-
-    public bool DeleteFlag { get; set; }
-
-    public DateTime StartTime { get; set; }
-
-    public DateTime EndTime { get; set; }
-
-    public DateTime CreatedTime { get; set; }
-
-    public DateTime? UpdatedTime { get; set; }
-
-    public virtual User Approver { get; set; } = null!;
-
-    public virtual User User { get; set; } = null!;
-
-    public virtual ICollection<Category> Categories { get; set; } = new List<Category>();
-
-    public virtual ICollection<Image> Images { get; set; } = new List<Image>();
-
-    public virtual ICollection<User> Users { get; set; } = new List<User>();
+    public virtual ICollection<Payment> Payments { get; set; } = new List<Payment>();
 }
