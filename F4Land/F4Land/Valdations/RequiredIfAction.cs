df242baa7560ffe8d29dcd12db7e371631a99d16
minhlel <155430170+minhlel@users.noreply.github.com>
2024-02-25 23:42:50 +0700

    update RequiredIfAction

--- F4Land/F4Land/Valdations/RequiredIfAction.cs
+++ F4Land/F4Land/Valdations/RequiredIfAction.cs
@@ -1,6 +1,32 @@
-﻿namespace F4Land.Valdations
+﻿using RealEstateAuction.DataModel;
+using RealEstateAuction.Enums;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace RealEstateAuction.Valdations
 {
-    public class RequiredIfAction
+    public class RequiredIfActionAttribute : ValidationAttribute
     {
+        private readonly PaymentType _requiredAction;
+
+        public RequiredIfActionAttribute(PaymentType requiredAction)
+        {
+            _requiredAction = requiredAction;
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var model = (PaymentDataModel)validationContext.ObjectInstance;
+
+            if (model.Action == PaymentType.TopUp && value == null)
+            {
+                return new ValidationResult(ErrorMessage);
+            }
+
+            return ValidationResult.Success;
+        }
     }
 }
+
+
