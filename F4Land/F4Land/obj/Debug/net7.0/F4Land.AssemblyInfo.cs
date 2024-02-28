35df3e7a983ab1d358e5915a0eb9b8cc176da733
toannnse171130 <155402953+toannnse171130@users.noreply.github.com>
2024-02-28 21:14:54 +0700

    update layout

--- F4Land/F4Land/obj/Debug/net7.0/F4Land.AssemblyInfo.cs
+++ F4Land/F4Land/obj/Debug/net7.0/F4Land.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("F4Land")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3ae085b0cadca171f389ebd2dce5ef33514c4c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("F4Land")]
 [assembly: System.Reflection.AssemblyTitleAttribute("F4Land")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
