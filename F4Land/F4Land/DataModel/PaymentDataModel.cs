ac4f9a18df93b7f724136fbae926bbee016cfcfc
minhlel <155430170+minhlel@users.noreply.github.com>
2024-03-12 20:45:33 +0700

    update DAO

--- F4Land/F4Land/DataModel/PaymentDataModel.cs
+++ F4Land/F4Land/DataModel/PaymentDataModel.cs
@@ -9,15 +9,17 @@ namespace RealEstateAuction.DataModel
         [Required(ErrorMessage = "Vui lòng nhập dữ liệu")]
         [Range(0, double.MaxValue, ErrorMessage = "Giá trị phải lớn hơn hoặc bằng 0.")]
         public int Amount { get; set; }
+
         [RequiredIfAction(PaymentType.TopUp, ErrorMessage = "Tài khoản nhận là bắt buộc")]
         public int BankId { get; set; }
+
         [Required(ErrorMessage = "Tài khoản giao dịch là bắt buộc")]
         public string UserAccountNumber { get; set; }
 
         [Required(ErrorMessage = "Ngân hàng là bắt buộc")]
         public string UserBankName { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Loại giao dịch là bắt buộc")]
         public PaymentType Action { get; set; }
     }
 }
