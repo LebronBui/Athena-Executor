14f6725a8c1d93fbf9027252e3c839427dd17913
toannnse171130 <155402953+toannnse171130@users.noreply.github.com>
2024-03-13 12:44:08 +0700

    update controllers

--- F4Land/F4Land/Controllers/StaffController.cs
+++ F4Land/F4Land/Controllers/StaffController.cs
@@ -267,6 +267,7 @@ namespace RealEstateAuction.Controllers
         [Route("details-auction")]
         public IActionResult DetailsAuction(int auctionId)
         {
+            ViewData["categories"] = categoryDAO.GetCategories();
             //check user login or not
             if (!User.Identity.IsAuthenticated)
             {
