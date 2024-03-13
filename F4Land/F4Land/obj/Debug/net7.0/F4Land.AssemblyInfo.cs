eb02295c9dcf4d1623ecc1f6eb37e3a95cabb2bd
MinhQuangSE171655 <144604032+MinhQuangSE171655@users.noreply.github.com>
2024-03-13 12:36:03 +0700

    update package

--- F4Land/F4Land/obj/Debug/net7.0/F4Land.AssemblyInfo.cs
+++ F4Land/F4Land/obj/Debug/net7.0/F4Land.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("F4Land")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1c34ca5232471d7808e28e2973cf29ee948fd4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b35a0de38e24c91c74a51df8c9ce5bd75662fb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("F4Land")]
 [assembly: System.Reflection.AssemblyTitleAttribute("F4Land")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
